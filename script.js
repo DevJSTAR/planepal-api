0c80ba98a7f920c1a6b89b81f12be35a3c89bdef
JSTAR <78903532+DevJSTAR@users.noreply.github.com>
2024-10-13 03:25:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.getElementById('fetchData').addEventListener('click', () => {
-    fetch('https://DevJSTAR.github.io/PlanePal-API/api/data.json')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('response').innerText = JSON.stringify(data, null, 2);
-        })
-        .catch(error => {
-            document.getElementById('response').innerText = 'Error: ' + error.message;
-        });
-});
+
